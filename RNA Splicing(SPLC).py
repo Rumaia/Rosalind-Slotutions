eb7d82f24a64e584cc0c2a3b8c171fbd2dd87a81
Rumaia <45440317+Rumaia@users.noreply.github.com>
2019-01-13 12:33:59 +0100

    Add files via upload

---
+++ RNA Splicing(SPLC).py
@@ -0,0 +1,18 @@
+from Methods import ReadFASTA, ProteinDictDNA
+
+dna_list = ReadFASTA('rosalind_splc.txt')
+exon = dna_list[0][1]
+
+# Remove the introns.
+for intron in dna_list[1:]:
+	exon = exon.replace(intron[1], '')
+
+# Translate the exons.
+dna_dict = ProteinDictDNA()
+exon_protein = ''
+for index in range(0, len(exon), 3):
+	exon_protein += dna_dict[exon[index:index+3]] if dna_dict[exon[index:index+3]]  != 'Stop' else ''
+
+print exon_protein
+with open('output_SPLC.txt', 'w') as output_data:
+	output_data.write(exon_protein)
